ffb77f0d27234d7449396db5e17dee08f42454d3
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-12-11 20:30:48 +0300

    feat: construct home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,29 @@
-import { CreatePost } from "../components/CreatePost/CreatePost";
+import { CommunitiesSection } from "../components/CommunitiesSection/CommunitiesSection";
+import { CreatePostSection } from "../components/CreatePostSection/CreatePostSection";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
+import { PostCard } from "../components/PostCard/PostCard";
+import { SuggestedPeopleSection } from "../components/SuggestedPeopleSection/SuggestedPeopleSection";
+
+const posts = ["1", "2", "3"];
 
 export function Home() {
   return (
     <>
       <Header></Header>
       <main>
-        <CreatePost></CreatePost>
+        <div className="main-content">
+          <CreatePostSection></CreatePostSection>
+          <div className="posts-list">
+            {posts.map((_, index) => (
+              <PostCard key={index}></PostCard>
+            ))}
+          </div>
+        </div>
+        <div className="sections">
+          <SuggestedPeopleSection></SuggestedPeopleSection>
+          <CommunitiesSection></CommunitiesSection>
+        </div>
       </main>
       <Footer></Footer>
     </>
