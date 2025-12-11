2004976923063d1e57e4d046a89723a9725df0f4
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-12-11 20:27:03 +0300

    feat: add section for creating posts

--- src/components/CreatePostSection/CreatePostSection.tsx
+++ src/components/CreatePostSection/CreatePostSection.tsx
@@ -1,12 +1,12 @@
-import "./CreatePost.css";
-export function CreatePost() {
+import "./CreatePostSection.css";
+export function CreatePostSection() {
   return (
-    <section className="create-post-section">
+    <article className="create-post-section">
       <div className="create-post-info">
         <div className="create-post-image"></div>
         <p>What's happening?</p>
       </div>
       <button>Tell everyone</button>
-    </section>
+    </article>
   );
 }
