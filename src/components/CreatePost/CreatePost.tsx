cdf6debd27ac31dcef368bbb63ffe659902f155b
Veronika Kashlej <veronika.kashlei@mail.ru>
2025-12-11 13:13:12 +0300

    feat: add create post section

---
+++ src/components/CreatePost/CreatePost.tsx
@@ -0,0 +1,12 @@
+import "./CreatePost.css";
+export function CreatePost() {
+  return (
+    <section className="create-post-section">
+      <div className="create-post-info">
+        <div className="create-post-image"></div>
+        <p>What's happening?</p>
+      </div>
+      <button>Tell everyone</button>
+    </section>
+  );
+}
