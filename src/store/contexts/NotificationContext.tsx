5113dbeb1c5c9dda41881f6d5e924f36f127c67b
Veronika Kashlej <veronika.kashlei@mail.ru>
2026-01-22 23:38:44 +0300

    feat: notification closes after 3s automatically

--- src/store/contexts/NotificationContext.tsx
+++ src/store/contexts/NotificationContext.tsx
@@ -1,52 +1,69 @@
-import { createContext, ReactNode, useContext, useState } from "react";
-import { CustomNotificationProps, CustomNotificationType } from "../types";
-import { CustomNotification } from "../../components/Notification/Notification";
+import {
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState,
+} from 'react';
+import { CustomNotificationProps, CustomNotificationType } from '../types';
+import { CustomNotification } from '../../components/Notification/Notification';
 
 interface CustomNotificationContextType {
-  showCustomNotification: (
-    message: string,
-    type: CustomNotificationType
-  ) => void;
+    showCustomNotification: (
+        message: string,
+        type: CustomNotificationType
+    ) => void;
 }
 
 const CustomNotificationContext = createContext<
-  CustomNotificationContextType | undefined
+    CustomNotificationContextType | undefined
 >(undefined);
 
 export const useCustomNotification = () => {
-  const context = useContext(CustomNotificationContext);
-  if (!context) {
-    throw new Error("useNotification must be used within NotificationProvider");
-  }
-  return context;
+    const context = useContext(CustomNotificationContext);
+    if (!context) {
+        throw new Error(
+            'useNotification must be used within NotificationProvider'
+        );
+    }
+    return context;
 };
 
 interface CustomNotificationProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export const CustomNotificationProvider = ({
-  children,
+    children,
 }: CustomNotificationProviderProps) => {
-  const [notification, setNotification] =
-    useState<CustomNotificationProps | null>(null);
+    const [notification, setNotification] =
+        useState<CustomNotificationProps | null>(null);
 
-  const showCustomNotification = (
-    message: string,
-    type: CustomNotificationType
-  ) => {
-    setNotification({ message, type });
-  };
+    const showCustomNotification = (
+        message: string,
+        type: CustomNotificationType
+    ) => {
+        setNotification({ message, type });
+    };
 
-  return (
-    <CustomNotificationContext value={{ showCustomNotification }}>
-      {children}
-      {notification && (
-        <CustomNotification
-          message={notification.message}
-          type={notification.type}
-        />
-      )}
-    </CustomNotificationContext>
-  );
+    const handleClose = () => {
+        setNotification(null);
+    };
+
+    useEffect(() => {
+        setTimeout(handleClose, 3000);
+    });
+
+    return (
+        <CustomNotificationContext value={{ showCustomNotification }}>
+            {children}
+            {notification && (
+                <CustomNotification
+                    message={notification.message}
+                    type={notification.type}
+                    onClose={handleClose}
+                />
+            )}
+        </CustomNotificationContext>
+    );
 };
