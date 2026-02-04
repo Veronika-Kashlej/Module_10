c6f8bcacfbb29dce42395bac84b983e4808add7a
Veronika Kashlej <veronika.kashlei@mail.ru>
2026-02-05 01:10:36 +0300

    fix: private root fix

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,8 +1,13 @@
-import { Navigate, Outlet } from 'react-router';
+import { Outlet, Route } from 'react-router';
 import { useAuth } from '../../utils/hooks/useAuth';
+import SignIn from '../../pages/SignIn/SignIn';
 
 export function PrivateRoute() {
     const { isAuthenticated, isLoading } = useAuth();
     if (isLoading) return null;
-    return isAuthenticated ? <Outlet /> : <Navigate to="/sign-in" />;
+    return isAuthenticated ? (
+        <Outlet />
+    ) : (
+        <Route path="/sign-in" element={<SignIn />} />
+    );
 }
